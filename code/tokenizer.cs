c85efd506e6ea430aecbb4676ab17fe9c5e1f7d3
Thorsten Kleppe <53048236+grensen@users.noreply.github.com>
2024-01-15 13:26:12 +0100

    Update tokenizer.cs

--- code/tokenizer.cs
+++ code/tokenizer.cs
@@ -3,19 +3,22 @@
 // NuGet\Install-Package Microsoft.DeepDev.TokenizerLib -Version 1.3.3
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+// https://jamesmccaffrey.wordpress.com/2024/01/15/using-news-articles-to-train-large-language-models/
 // https://github.com/stephentoub/Tokenizer
 // https://www.nuget.org/packages/Microsoft.DeepDev.TokenizerLib/
 
 using Microsoft.DeepDev;
+
+Console.WriteLine("Begin gpt-4 tokenizer demo");
 
 var IM_START = "<|im_start|>";
 var IM_END = "<|im_end|>";
 var specialTokens = new Dictionary<string, int> { { IM_START, 100264 }, { IM_END, 100265 }, };
 ITokenizer tokenizer = await TokenizerBuilder.CreateByModelNameAsync("gpt-4", specialTokens);
 
-string text = "Hello World";
+string text = "The future of AI is impredictable."; // "Hello World";
 // demo 1 encode tokens
-StringToTokens(IM_START + text + IM_END);
+StringToTokens(text);  //StringToTokens(IM_START + text + IM_END);
 // demo 2 decode tokens
 PrintDecodedTokens(0, 5);
 PrintDecodedTokens(9905, 5);
@@ -45,7 +48,7 @@ void StringToTokens(string text)
     }
 
     var decoded = tokenizer.Decode(tokens.ToArray());
-    Console.WriteLine("\nDecoded tokens\n" + decoded);
+    Console.WriteLine("\nDecoded tokens:\n" + decoded);
 }
 void CreateOutputTokensFile(string outputPath, int length)
 {
